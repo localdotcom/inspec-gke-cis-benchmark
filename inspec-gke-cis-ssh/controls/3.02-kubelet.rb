bc902522a2cbefd3d53c1a9af41e34a9ca24ea9b
Sergey Yelenski <sergey.yelenski@warecorp.com>
2022-12-29 15:59:36 +0200

    3.02-kubelet profile changed

--- inspec-gke-cis-ssh/controls/3.02-kubelet.rb
+++ inspec-gke-cis-ssh/controls/3.02-kubelet.rb
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# test
 
 gcp_project_id = input('gcp_project_id')
 cis_version = input('cis_version')
